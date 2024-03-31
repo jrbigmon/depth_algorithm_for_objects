56b141b6792d2c87893511075658927dd4a00c20
jrbigmon <juniorterriaga0@gmail.com>
2024-03-30 23:31:34 -0300

    feat: depth algorithm for objects

---
+++ index.js
@@ -0,0 +1,71 @@
+const algorithmObjectMap = () => {
+  const getListOfProperties = (object) => {
+    const listOfProperties = [];
+
+    const mapObject = (object, father = []) => {
+      const listOfTemporaryObjects = [];
+
+      Object.entries(object)?.forEach(([key, value]) => {
+        if (typeof value !== "object") {
+          if (!father?.length) {
+            listOfProperties.push(key);
+          } else {
+            listOfProperties.push([...father, key]);
+          }
+        } else {
+          listOfTemporaryObjects.push([
+            [...(father?.length ? father : []), key],
+            value,
+          ]);
+        }
+      });
+
+      listOfTemporaryObjects?.forEach(([keys, object]) =>
+        mapObject(object, keys)
+      );
+    };
+
+    mapObject(object);
+
+    return listOfProperties;
+  };
+
+  const getRelativePathByValue = (object, valueToSearch) => {
+    let relativePath = null;
+
+    const mapObjectAndSearch = (object, father = []) => {
+      const listOfTemporaryObjects = [];
+
+      const objectEntries = Object.entries(object);
+
+      for (const [key, value] of objectEntries) {
+        if (typeof value !== "object") {
+          if (value === valueToSearch) {
+            relativePath = [...father, key];
+            break;
+          }
+        } else {
+          listOfTemporaryObjects.push([
+            [...(father?.length ? father : []), key],
+            value,
+          ]);
+        }
+      }
+
+      listOfTemporaryObjects?.forEach(([keys, object]) => {
+        if (!relativePath?.length) mapObjectAndSearch(object, keys);
+      });
+    };
+
+    mapObjectAndSearch(object);
+
+    return relativePath;
+  };
+
+  return {
+    getListOfProperties,
+    getRelativePathByValue,
+  };
+};
+
+module.exports = algorithmObjectMap;
